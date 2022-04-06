ee63afc665fc81cba0fbe9ec69b577e2ac9e45a7
Anya Michaelsen <anya.michaelsen2@gmail.com>
2022-04-06 23:01:23 +0000

    Write and run bash script to load desired files

---
+++ repo-basic-setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.github.com/losc-tutorial/LOSC_Event_tutorial/master/GW150914_4_template.hdf5
+wget https://raw.github.com/losc-tutorial/LOSC_Event_tutorial/master/L-L1_LOSC_4_V2-1126259446-32.hdf5
+wget https://raw.github.com/losc-tutorial/LOSC_Event_tutorial/master/H-H1_LOSC_4_V2-1126259446-32.hdf5
+
+wget https://raw.githubusercontent.com/losc-tutorial/LOSC_Event_tutorial/master/index.ipynb
+wget https://raw.githubusercontent.com/losc-tutorial/LOSC_Event_tutorial/master/readligo.py
