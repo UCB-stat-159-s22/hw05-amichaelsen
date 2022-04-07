096967caa46fa9f4817069911936632a0f44a02a
Anya Michaelsen <anya.michaelsen2@gmail.com>
2022-04-07 00:23:35 +0000

    Add BBH json to script and load into repo

--- repo-basic-setup.sh
+++ repo-basic-setup.sh
@@ -1,3 +1,4 @@
+wget https://raw.githubusercontent.com/losc-tutorial/LOSC_Event_tutorial/master/BBH_events_v3.json
 wget https://raw.github.com/losc-tutorial/LOSC_Event_tutorial/master/GW150914_4_template.hdf5
 wget https://raw.github.com/losc-tutorial/LOSC_Event_tutorial/master/L-L1_LOSC_4_V2-1126259446-32.hdf5
 wget https://raw.github.com/losc-tutorial/LOSC_Event_tutorial/master/H-H1_LOSC_4_V2-1126259446-32.hdf5
